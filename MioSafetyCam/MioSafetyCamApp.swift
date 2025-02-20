fa649e9f388909b2aaeb5f4706d1b6950471366d
Benjamin.Ko <designerko1215@gmail.com>
2025-02-20 10:27:11 +0800

    Initial Commit

---
+++ MioSafetyCam/MioSafetyCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MioSafetyCamApp.swift
+//  MioSafetyCam
+//
+//  Created by hmi-ben on 2025/2/20.
+//
+
+import SwiftUI
+
+@main
+struct MioSafetyCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MioSafetyCamTests/MioSafetyCamTests.swift
@@ -0,0 +1,17 @@
+//
+//  MioSafetyCamTests.swift
+//  MioSafetyCamTests
+//
+//  Created by hmi-ben on 2025/2/20.
+//
+
+import Testing
+@testable import MioSafetyCam
+
+struct MioSafetyCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
